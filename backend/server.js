6f1b6b75b41608e5e2ef13bc44fee592057f7af6
Clovis Fong <fongclovis@hotmail.com>
2022-12-11 22:18:25 +0800

    added dependencies and created chat routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
 const express = require("express")
+const { chats } = require('./data/data')
+const dotenv = require('dotenv')
 
 const app = express()
+dotenv.config()
+const PORT = process.env.PORT ?? 3000
+
 
 app.get('/', (req, res) => {
     res.send('Chat app started')
 })
 
+app.get('/api/chat', (req, res) => {
+    res.send(chats)
+})
+
+app.get('/api/chat/:id', (req, res) => {
+    const { id } = req.params
+
+    const singleChat = chats.find(chat => chat._id === id)
+    res.send(singleChat)
+})
 
 
-app.listen(3000, console.log('server started'))
+app.listen(PORT, console.log(`express started on ${PORT}`))
