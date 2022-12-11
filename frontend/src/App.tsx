bd3779cb7e144f54681e4230148051c54308ef34
Clovis Fong <fongclovis@hotmail.com>
2022-12-11 22:43:03 +0800

    set up frontend with vite-react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div >
+      <h1 className='text-4xl font-bold underline text-blue'>Hello</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'blue': '#1fb6ff',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
